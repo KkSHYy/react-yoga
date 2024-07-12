f1521a970b76b9448303303f5eef3603144e0790
Sia <sagun@ismt.edu.np>
2024-07-12 08:52:27 +0545

    ch

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,58 @@
 import React from 'react'
+import { meal, recipe } from '../dummy/data'
 
 const Home = () => {
 
-  const numbers = [22,44,66,88];
+
 
   return (
-    <div>
-  {numbers.map((e,i)=>
+    <div className='p-2 space-y-3'>
+  <h1 className='text-2xl font-bold'>{recipe.name}</h1>
+  <div className='img'>
+    <img className='h-[200px] w-[200px]' src={recipe.image} alt="" />
+  </div>
+  <ol>
+  <h1 className='text-xl font-bold'>Ingredients</h1>
+   {
+    recipe.ingredients.map((ing,i)=>{
+      return <li key={i}>{`${i+1}.${ing}`}</li>
+    })
+   }
+  </ol>
+ 
+  <ol>
+  <h1 className='text-xl font-bold'>Instructions</h1>
+   {
+    recipe.instructions.map((ins,i)=>{
+      return <li key={i}>{`${i+1}.${ins}`}</li>
+    })
+   }
+  </ol>
+
+  <h1 className='text-2xl font-bold'>{meal.strMeal}</h1>
+  <ol>
+  <h1 className='text-xl font-bold'>Instructions</h1>
   {
-    return <h1 key={i}>{'hello ${e} sddfg'}</h1>
-  })}
+    meal.strInstructions.split('.').map((ins,i)=>{
+      return <li key={i}>{`${i+1}.${ins}`}</li>
+  })
+  }
+  </ol>
+
+  <ol>
+  <h1 className='text-xl font-bold'>Category</h1>
+  {
+    meal.strCategory.split((you,i)=>
+    {
+      return <li key={i}>{`${i+1}.${you}`}</li>
+    })
+  }
+  </ol>
   
-    <h1>This is a Home Page</h1>
+    {/* <h1>This is a Home Page</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis sequi amet facere ipsa laborum rem nesciunt totam sapiente aliquam veritatis facilis, eos dignissimos atque placeat sunt hic repellendus natus asperiores.</p>
-    <p>-----------------------------------------------------------------------------------------------------------------------------</p>
+    <p>-----------------------------------------------------------------------------------------------------------------------------</p> */}
+    
     </div>
   )
 }
